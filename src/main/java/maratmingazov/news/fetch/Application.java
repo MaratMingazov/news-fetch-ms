30f9df34c5982682339190e3d1a726e06ebf5fd3
Marat Mingazov <marat_mingazov@epam.com>
2022-08-28 16:50:46 +0300

    Add webClient configuratuin

--- src/main/java/maratmingazov/news/fetch/Application.java
+++ src/main/java/maratmingazov/news/fetch/Application.java
@@ -2,8 +2,10 @@ package maratmingazov.news.fetch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("maratmingazov.news.fetch.config")
 public class Application {
 
 	public static void main(String[] args) {
