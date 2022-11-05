4ebecffbfc63fff5b1069b09265749eaa743794e
Marat Mingazov <marat_mingazov@epam.com>
2022-11-05 16:24:12 +0300

    Delete wordRepository

--- src/main/java/maratmingazov/news/fetch/repository/WordRepository.java
+++
@@ -1,7 +0,0 @@
-package maratmingazov.news.fetch.repository;
-
-import maratmingazov.news.fetch.model.mongodb.MongoWord;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WordRepository extends MongoRepository<MongoWord, String> {
-}
